3e53d64bcc20013d2710172dc3299b4fe3452ff3
Jason Christian H <nakamarujc@gmail.com>
2020-05-22 15:29:03 +0700

    Changed RandomUtil namespace into damn static function in class

--- UtilitarianAirlines/UtilitarianAirlines/Algorithms/RandomUtil.h
+++ UtilitarianAirlines/UtilitarianAirlines/Algorithms/RandomUtil.h
@@ -5,25 +5,25 @@
 #include <iostream>
 
 using namespace std;
-namespace Rand {
+class Rand {
 // Generate a random number.
-int randomRangeNum(int min, int max) {
+static int randomRangeNum(int min, int max) {
     return ((rand() % (max - min)) + min);
 }
 
-int clampNum(int val, int min, int max) {
+static int clampNum(int val, int min, int max) {
     if (val < min) { return min; }
     else if (val > max) { return max; }
     else { return val; }
 }
 
 // Generate a random number with more precision.
-double randomRangeNum(double min, double max) {
+static double randomRangeNum(double min, double max) {
     double seeder = (double)rand() / (double)rand();
     return (fmod(((double)rand() + seeder), (max - min)) + min);
 }
 
-double randomNormalNum(double min, double max, double multiplier) {
+static double randomNormalNum(double min, double max, double multiplier) {
     double val = 0;
 
     for (int i = 0; i < (int)(ceil(multiplier)); i++) {
